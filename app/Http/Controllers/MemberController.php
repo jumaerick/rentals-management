40f05bc9bb813bf50fe114d6c2d62d51c26be764
Juma Erick <jumaaerick@gmail.com>
2024-08-18 22:51:13 +0300

    first-commit

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    //
+    public function register()
+    {
+
+        return view('users.register');
+        //
+    }
+
+    
+
+
+}
