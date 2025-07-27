52ff897613074b9ab9472370089ad51bc4ad302a
juma erick <jumaaerick@gmail.com>
2025-07-27 23:09:45 +0300

    load-more-data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Post;
+use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 
@@ -26,6 +27,31 @@ class PostController extends Controller
 
   
         return view('posts.index')->with(['posts'=>$posts, 'morePosts'=>$morePosts, 'lastPage'=>$lastPage]);
+    }
+
+    public function showDueDates(){
+        $dueDates = collect([
+        (object)[
+            'id' => 1,
+            'title' => 'Math 101 Homework',
+            'due_date' => Carbon::now()->addDays(3)->setHour(23)->setMinute(59),
+            'description' => 'Complete exercises 1 to 10.',
+        ],
+        (object)[
+            'id' => 2,
+            'title' => 'Physics Lab Report',
+            'due_date' => Carbon::now()->addWeek()->setHour(17)->setMinute(0),
+            'description' => 'Submit lab report on pendulum experiment.',
+        ],
+        (object)[
+            'id' => 3,
+            'title' => 'English Essay',
+            'due_date' => Carbon::now()->addDays(5)->setHour(20)->setMinute(0),
+            'description' => 'Write an essay on modern poetry.',
+        ],
+    ]);
+
+    return view('posts.due_dates', compact('dueDates'));
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Jobs\SendWelcomeEmailJob;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,6 +38,49 @@ Mail::raw('This is a test email using Mailtrap!', function ($message) {
 
 Route::get('/cert', [App\Http\Controllers\HomeController::class, 'generatePdf'])->name('generatePdf');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/events', [PostController::class, 'showDueDates'])->name('due_dates');
+Route::get('/events/{id}/download-ics', function ($id) {
+    $dummyEvents = [
+        1 => [
+            'title' => 'Math 101 Homework',
+            'due_date' => Carbon::now()->addDays(3)->setHour(23)->setMinute(59),
+            'description' => 'Complete exercises 1 to 10.',
+        ],
+        2 => [
+            'title' => 'Physics Lab Report',
+            'due_date' => Carbon::now()->addWeek()->setHour(17)->setMinute(0),
+            'description' => 'Submit lab report on pendulum experiment.',
+        ],
+        3 => [
+            'title' => 'English Essay',
+            'due_date' => Carbon::now()->addDays(5)->setHour(20)->setMinute(0),
+            'description' => 'Write an essay on modern poetry.',
+        ],
+    ];
+
+    if (!isset($dummyEvents[$id])) {
+        abort(404);
+    }
+
+    $event = $dummyEvents[$id];
+
+    $icsContent = "BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//YourApp//EN
+BEGIN:VEVENT
+UID:{$id}@yourapp.com
+DTSTAMP:" . now()->format('Ymd\THis\Z') . "
+DTSTART:" . $event['due_date']->format('Ymd\THis\Z') . "
+DTEND:" . $event['due_date']->copy()->addHour()->format('Ymd\THis\Z') . "
+SUMMARY:{$event['title']}
+DESCRIPTION:{$event['description']}
+END:VEVENT
+END:VCALENDAR";
+
+    return response($icsContent, 200)
+        ->header('Content-Type', 'text/calendar')
+        ->header('Content-Disposition', 'attachment; filename="event-' . $id . '.ics"');
+})->name('events.download-ics');
 
 Route::get('/partners', [App\Http\Controllers\HomeController::class, 'partners'])->name('partners');
 
